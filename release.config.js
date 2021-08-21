d65ce64aa4e147312446a33db373c41a3ac9f3c3
repo-visualizer <repo-visualizer@users.noreply.github.com>
2021-08-21 17:59:20 +0000

    Repo visualizer: updated diagram

--- release.config.js
+++ release.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-  dryRun: false,
+  branches: [
+    "+([0-9])?(.{+([0-9]),x}).x",
+    "main",
+    "master",
+    "next",
+    "next-major",
+    {
+      name: "beta",
+      prerelease: true,
+    },
+    {
+      name: "alpha",
+      prerelease: true,
+    },
+  ],
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
