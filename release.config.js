eb829828b314d42593620197a0efc21e4aa78385
Unbuttun <unbuttun@gmail.com>
2021-08-21 23:28:46 +0530

    fix(actions): 🐛 Trying to fix the release action

--- release.config.js
+++ release.config.js
@@ -1,43 +1,23 @@
 module.exports = {
-  branches: [
-    "+([0-9])?(.{+([0-9]),x}).x",
-    "main",
-    "master",
-    "next",
-    "next-major",
-    {
-      name: "beta",
-      prerelease: true,
-    },
-    {
-      name: "alpha",
-      prerelease: true,
-    },
-  ],
+  dryRun: false,
   plugins: [
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
     [
-      "@semantic-release/npm",
+      "@semantic-release/changelog",
       {
-        tarballDir: "release",
+        changelogFile: "CHANGELOG.md",
       },
     ],
-    [
-      "@semantic-release/github",
-      {
-        assets: "release/*.tgz",
-      },
-    ],
+    "@semantic-release/npm",
+    "@semantic-release/github",
     [
       "@semantic-release/git",
       {
-        assets: ["package.json"],
+        assets: ["CHANGELOG.md", "package.json", "pnpm-lock.yaml"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
     ],
   ],
-  preset: "angular",
 };
